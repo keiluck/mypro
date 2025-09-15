617db2db92e2ec2b3b0876ab19a7d4111cfee7f5
keiluck <keiluck365@gmail.com>
2025-09-15 20:42:52 +0900

    dd

--- lady/script.js
+++
@@ -1,6 +0,0 @@
-function addToCart(item){
-  let cart = JSON.parse(localStorage.getItem("cart") || "[]");
-  cart.push(item);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert(item + " 已加入购物车！");
-}
